7e3da7979df100345c973f45b3442dfccc4d9ed4
luisxx123 <luis252001@hotmail.com>
2022-04-15 21:44:23 -0300

    Add some libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <h1>Hello World</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
