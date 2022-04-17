92ea0bbeb7a57d064304d59f37b2f82d402af0c8
luisxx123 <luis252001@hotmail.com>
2022-04-17 20:06:22 -0300

    Add responsivity to login page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="container-fluid d-flex p-0 home">
       <section className="container-fluid d-flex justify-content-end left-container">
-        <div className="d-flex flex-column h-100 justify-content-center left-container-items w-75">
+        <div className="d-flex flex-column h-100 justify-content-center left-container-items mx-sm-3 w-75">
           <img className="logo" alt="logo.svg" src={logo} />
           <h2 className="text-left text-white w-75">
             {CONSTANTS.PARK_WAS_NEVER_THAT_EASY}
@@ -16,11 +16,11 @@ export default function Home() {
         </div>
       </section>
       <section className="container-fluid right-container">
-        <div className="align-items-center d-flex flex-column gap-2 justify-content-center right-container-content">
+        <div className="align-items-center d-flex flex-column gap-2 h-100 justify-content-center mt-3 right-container-content">
           <div className="right-container-title">
             <h2>{CONSTANTS.ACCESS_AS}</h2>
           </div>
-          <div className="d-grid gap-3 col-6 mx-auto right-container-body">
+          <div className="d-grid gap-3 col-lg-6 right-container-body">
             <button type="button" className="btn btn-outline-dark">
               <span className="align-middle w-100 h-100">
                 {CONSTANTS.ADMIN}
