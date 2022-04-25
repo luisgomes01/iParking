3bb2d60b30a6f7b4a3d96237291aaa17351c115e
Luís Gomes <luis.gomes@liferay.com>
2022-04-25 18:56:39 -0300

    WIP Add admin form

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,11 +1,95 @@
-import React from 'react'
+import './admin.scss'
 
-import Menu from '../../layout/Menu/Menu'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { CONSTANTS } from '../../utils/constants'
 
 export default function Admin() {
+  const navigate = useNavigate()
+
   return (
     <>
-      <Menu />
+      <main className="admin container-fluid d-flex flex-column">
+        <p className="admin-title h1 text-center mt-3">
+          {CONSTANTS.ADMIN_ROUTE.BUSINESS_RULES}
+        </p>
+        <section className="admin-form align-items-center col-lg-6 col-md-6 col-sm-6 container-fluid d-flex flex-md-row flex-lg-row flex-sm-column flex-column justify-content-center w-100">
+          <div className="align-items-center col-xl-2 d-flex flex-column justify-content-center">
+            <label className="form-label mb-2">
+              {CONSTANTS.ADMIN_ROUTE.GRACE_PERIOD}
+            </label>
+            <input
+              className="form-control iparking-input text-center"
+              placeholder="ex. 3"
+              type="text"
+            />
+            <label className="form-label mb-2 mt-2">
+              {CONSTANTS.ADMIN_ROUTE.FIXED_TIME}
+            </label>
+            <input
+              className="form-control iparking-input text-center"
+              placeholder="Tempo fixo"
+              type="text"
+            />
+            <label className="form-label mb-2 mt-2">
+              {CONSTANTS.ADMIN_ROUTE.TIME_FIXED_VALUE}
+            </label>
+            <input
+              className="form-control iparking-input text-center"
+              placeholder="Valor por tempo fixo"
+              type="text"
+            />
+          </div>
+          <div className="align-items-center col-xl-2 d-flex flex-column justify-content-center">
+            <label className="form-label mb-2">
+              {CONSTANTS.ADMIN_ROUTE.HOUR_EXCEEDED}
+            </label>
+            <input
+              className="form-control iparking-input text-center"
+              placeholder="Valor por hora excedida"
+              type="text"
+            />
+            <label className="form-label mb-2 mt-2">
+              {CONSTANTS.ADMIN_ROUTE.DAILY_VALUE}
+            </label>
+            <input
+              className="form-control iparking-input text-center"
+              placeholder="Valor da diária"
+              type="text"
+            />
+            <label className="form-label mb-2 mt-2">
+              {CONSTANTS.ADMIN_ROUTE.MONTHLY_VALUE}
+            </label>
+            <input
+              className="form-control iparking-input text-center"
+              placeholder="Valor mensal"
+              type="text"
+            />
+          </div>
+        </section>
+        <div className="admin-form align-items-center col-lg-6 col-md-6 col-sm-6 container-fluid d-flex flex-md-row flex-lg-row flex-sm-column flex-column justify-content-center w-100">
+          <div className="col-2">
+            <button
+              className="btn btn-outline-dark w-100"
+              onClick={() => navigate('/')}
+            >
+              Cancelar
+            </button>
+          </div>
+          <div className="col-2">
+            <button
+              className="btn btn-outline-primary w-100"
+              onClick={() => {
+                navigate('/')
+                alert('Salvo com sucesso')
+              }}
+            >
+              Salvar
+            </button>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,15 @@
 export const CONSTANTS = {
   ACCESS_AS: 'Acessar como',
   ADMIN: 'Administrador',
+  ADMIN_ROUTE: {
+    BUSINESS_RULES: 'Regras de negócio',
+    DAILY_VALUE: 'Valor da diária',
+    FIXED_TIME: 'Tempo fixo',
+    GRACE_PERIOD: 'Tempo de carência',
+    HOUR_EXCEEDED: 'Valor por hora excedida',
+    MONTHLY_VALUE: 'Valor mensal',
+    TIME_FIXED_VALUE: 'Valor por tempo fixo'
+  },
   CUSTOMER: 'Cliente',
   PARK_WAS_NEVER_THAT_EASY: 'Estacionar nunca foi tão fácil.'
 }
