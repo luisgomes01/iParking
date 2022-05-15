ff9c659869fee6aa988c984c84736efeac2f7342
cristianoalves12 <cristianoalf012@gmail.com>
2022-05-15 18:24:38 -0300

    add payment code

--- src/pages/application/PaymentCode/PaymentCode.jsx
+++ src/pages/application/PaymentCode/PaymentCode.jsx
@@ -7,13 +7,18 @@ export default function PaymentCode() {
   return (
     <Home>
       <section className="d-flex align-items-center justify-content-between flex-column">
-        <div className="payment-code-title">
-          <h2 className="pt-3">PaymentCode</h2>
+        <div className="payment-code-title mb-4">
+          <h2 className="pt-3">Código de barras</h2>
         </div>
         <div>
           <img src={barcode} alt="Generated barcode." />
         </div>
-        <div>INfo</div>
+        <div className="information d-flex align-items-center flex-column mt-4">
+          <p>Entrada: <span>8:00 hrs</span></p>
+          <p>Saída: <span>8:00 hrs</span></p>
+          <p>Permaneência: <span>9:00 hrs</span></p>
+          <p>Valor: <span>R$XX,XX</span></p>
+        </div>
       </section>
     </Home>
   )
