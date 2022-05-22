bace61666b55d74b54fded27471b7c7ef3d55a9a
Luís Gomes <luis.gomes@liferay.com>
2022-05-21 21:56:14 -0300

    Fix signup page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,6 +7,9 @@ import Availability from '../pages/application/Availability/Availability'
 import GeneratePayment from '../pages/application/GeneratePayment/GeneratePayment'
 import PaymentCode from '../pages/application/PaymentCode/PaymentCode'
 import Customer from '../pages/customer/Customer'
+import SignupAuto from '../pages/customer/signup/SignupAuto'
+import SignupCard from '../pages/customer/signup/SignupCard'
+import SignupUser from '../pages/customer/signup/SignupUser'
 import Home from '../pages/home/Home'
 
 export default function MainRoutes() {
@@ -17,6 +20,11 @@ export default function MainRoutes() {
           <Route index element={<Admin />} />
         </Route>
         <Route path="/customer" element={<Customer />} />
+        <Route path="/signup" element={<Index />}>
+          <Route index element={<SignupUser />} />
+          <Route path="auto" element={<SignupAuto />} />
+          <Route path="card" element={<SignupCard />} />
+        </Route>
         <Route path="/" element={<Home />} />
         <Route path="/application" element={<Index />}>
           <Route index element={<Availability />} />
